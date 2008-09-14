c7088caab052ecdc48179bd5a5063c31ad7a8b10
Brian Preston <brian8181@gmail.com>
2008-09-14 23:33:38 +0000

    eclipse project settings
    git-svn-id: file:///home/brian/mig/music_web/trunk@47 356dc8ab-2228-4c92-b7b5-0cb1f9e44984

--- lyrics.php
+++ lyrics.php
@@ -1,45 +1,44 @@
-<?php 
-include_once("./config/config.php");
-			?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<!-- headers -->
-	<head>
-		<title>Song Lyrics</title>
-		<meta name="generator" content="Bluefish 1.0.7"/>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" type="text/css" href="./css/<?php echo($style); ?>" />
-	</head>
-	<body>
-	<div class="text_area">
-		<?php include("./module/login_greeting.php"); ?>
-		<br />
-			<div class="box" style="text-align: center">
-			<center>
-				<h1><em>Song Lyrics</em></h1>
-			</center>
-			</div>
-			<br />	
-			
 <?php
-include("./module/top_toolbar.php"); 
-			?>
-			
-			<hr />
+include_once("./config/config.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"
+	lang="en">
+<!-- headers -->
+<head>
+<title>Song Lyrics</title>
+<meta name="generator" content="Bluefish 1.0.7" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css"
+	href="./css/<?php echo($style); ?>" />
+</head>
+<body>
+<div class="text_area"><?php include("./module/login_greeting.php"); ?>
+<br />
+<div class="box" style="text-align: center">
+<center>
+<h1><em>Song Lyrics</em></h1>
+</center>
+</div>
+<br />
+
+<?php
+include("./module/top_toolbar.php");
+?>
+
+<hr />
 <?php
 $db = mysql_connect($db_address, $db_user_name, $db_password);
 mysql_select_db($db_name, $db);
 mysql_query("SET NAMES 'utf8'");
-
 /// get song id
 $sid = isset($_GET['sid']) ? $_GET['sid'] : null;
-			?>
-			
-			<center>
-			
+?>
+
+<center>
 <?php
 if( isset( $sid ) )
-{	
+{
 	$sql = "SELECT title, lyrics from song WHERE song.id=$sid";
 	$result = mysql_query($sql);
 	$row = mysql_fetch_array($result, MYSQL_NUM);
@@ -50,21 +49,14 @@ if( isset( $sid ) )
 	echo( $lyrics );
 	mysql_close($db);
 }
-			?>
-			
-			</center>
-			<br />
-			<hr />
-			
+?></center>
+<br />
+<hr />
+
 <?php
 include("./module/bottom_toolbar.php");
 include("./module/contact_info.php");
-			?>
-
-<?php
 include("./module/version.php");
-			?>
-			
-	</div>	
-	</body>
+?></div>
+</body>
 </html>
