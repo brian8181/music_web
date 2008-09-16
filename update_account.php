e0fa8aec6690f3a0d54ba166345319696af2e165
Brian Preston <brian8181@gmail.com>
2008-09-16 03:50:05 +0000

    git-svn-id: file:///home/brian/mig/music_web/trunk@55 356dc8ab-2228-4c92-b7b5-0cb1f9e44984

---
+++ update_account.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+include_once("./config/config.php");
+$user_name  = isset($_GET['user_name']) ? $_GET['user_name'] : null;
+			?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>Update Account</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="./css/<?php echo($style); ?>" />
+	</head>
+	<body>
+	<div class="text_area">
+		<center>
+		<table class="Main" width="100%"><tr><td class="Padded">
+			<div class="box" style="text-align: center">
+			<center>
+				<h1>Update Account</h1>
+			</center>
+			</div>
+			<br />	
+			<hr />
+			<center>
+			<form action="update_account.php" method="get">
+				<input type="hidden" name="submitted" value="true"/>
+				<table cellpadding="3">
+				<tr>
+					<td>User:&nbsp;</td><td><b><?php if (isset($user_name)) { echo $user_name; } ?></b></td>
+				</tr>
+				<tr>
+					<td>Password:&nbsp;</td>
+					<td><input type="password" name="orginal" value="<?php if (isset($orginal)) { echo $orginal; } ?>" /></td>
+				</tr>
+				<tr>
+					<td>New Password:&nbsp;</td>
+					<td><input type="password" name="password" value="<?php if (isset($password)) { echo $password; } ?>" /></td>
+				</tr>
+				<tr>
+					<td>Retype:&nbsp;</td>
+					<td><input type="password" name="password2" value="<?php if (isset($password2)) { echo $password2; } ?>" /></td>
+				</tr>
+				<tr>
+					<td>Full Name:&nbsp;</td>
+					<td><input type="text" name="full_name" value="<?php if (isset($full_name)) { echo $full_name; } ?>" /></td>
+				</tr>
+				<tr>
+					<td>Email:&nbsp;</td>
+					<td><input type="text" name="email" value="<?php if (isset($email)) { echo $email; } ?>" /></td>
+				</tr>
+				</table>
+				<input type="submit" value="Update" />			
+			</form>
+			</center>
+			<center>
+			<h3>
+<?php
+include("./php/functions.php");
+include("./admin/functions.php");
+
+$orginal    = isset($_GET['$orginal'])  ? $_GET['$orginal']  : null;
+$password   = isset($_GET['password'])  ? $_GET['password']  : null;
+$password2  = isset($_GET['password2']) ? $_GET['password2'] : null;
+$full_name  = isset($_GET['full_name']) ? $_GET['full_name'] : null;
+$email      = isset($_GET['email'])     ? $_GET['email']     : null;
+
+if( !empty($user_name) && !empty($password) && !empty($password2) && !empty($full_name) && !empty($email) )
+{
+	// TODO! validate user, password, email for length spaces & invalid chars
+	if( $password == $password2 )
+	{
+		if( validate_pass( $password ) )
+		{
+			$db = mysql_connect($admin_db_address, $admin_db_user_name, $admin_db_password);
+			mysql_select_db($admin_db_name, $db);
+			mysql_query("SET NAMES 'utf8'");
+			$user_name = mysql_real_escape_string($user_name);
+			$password = mysql_real_escape_string($password);
+			$full_name = mysql_real_escape_string($full_name);
+			$email = mysql_real_escape_string($email);
+			// make sure user dose not exsit
+			if(update_account( $user_name, $password, $full_name, $email, $db ))
+			{
+				echo( "Account updated for $full_name ($user_name)" );
+			}
+			else
+			{
+				echo( "Sorry account could not be updated plesae try agion later (unknown Error)." );
+			}	
+			mysql_close($db);
+		}
+		else
+		{
+			echo( "Password must be at least 7 characters and be both letters and numbers." );
+		}
+	}
+	else
+	{
+		echo( "Password do not match." );
+	}
+}
+else
+{
+	if(isset($submitted))
+		echo( "Invalid or missing fields" );
+	else
+		echo( "Please provide this information." );	
+}
+			?>
+			</h3>
+			</center>
+			<hr />
+			<br />
+			<!-- verison info -->
+			<em>
+			Version 1.0.0.1 Mon Dec 24 08:52:35 EST 2007 ~( Copyright © by Brian Preston (2007) )
+			</em>
+			<!-- End Page Table -->
+		</td></tr></table>
+		</center>
+		</div>
+	</body>
+</html>
---
+++ user_admin.php
@@ -0,0 +1,186 @@
+<?php
+session_start();
+include_once("./config/config.php");
+include_once("./php/functions.php");
+
+if( !isset( $_SESSION['_USER'] ) || !isset( $_SESSION['_GROUPS'] ) )
+{
+	$_SESSION['_PAGE'] = $_SERVER['PHP_SELF'];
+	header( "Location: ../login.php" );
+	exit();
+}
+
+$groups_loc = $_SESSION['_GROUPS']; 
+if(!array_key_exists('admin', $groups_loc))
+{
+	echo( "<div style=\"text-align: center\"><h3>Sorry, your are not authorized to access the page.</h3></div>" );	
+	echo( "<div style=\"text-align: center\"><a href=\"/index.php\"><i>www.bkp-online.com</i></a></div>" );
+	exit();
+}
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<!-- Headers -->
+<head>
+    <title>User Admin Page</title>
+    <link rel="stylesheet" type="text/css" href="./css/<?php echo($style); ?>" />
+    <script type="text/javascript">
+		function on_submit(form)  // intialize all values
+		{
+			// get check boxes
+			var frm = document.forms[2];
+			var count = frm.elements.length;
+			var user_group = "";
+			var cur_user = "";
+			for( var i = 0; i < count; ++i)
+			{
+				if(frm.elements[i].checked)
+				{
+					var str = frm.elements[i].name;
+					var pairs = str.split('-');
+					var user = pairs[0] ;
+					var group = pairs[1];
+					
+					if(cur_user != user)
+					{
+						if(cur_user == "")
+							user_group += user + ":" + group;
+						else
+							user_group += "-" + user + ":" + group;
+						cur_user = user;
+					}
+					else
+					{
+						user_group += "," + group;
+					}
+				}
+			}
+			form.elements['usr_grps'].value = user_group;
+			return true;
+		}
+    </script>
+    </head>
+	<body>
+		<div class="text_area">
+<?php 
+include("./module/login_greeting.php");
+	?>
+		<div class="box" style="text-align: center">
+			<h1><em>User Administration</em></h1>
+		</div>
+		<br />
+		<?php include("./module/top_toolbar.php"); ?>
+
+		<hr />
+		<br />
+	
+		<div style="text-align: center">
+		<form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="get" enctype="text/plain" >
+			<input type="text" name="filter" size="6" style="text-align: center;" />
+			<input type="submit" value="Filter" />
+		</form>
+		</div>
+		<br />
+		<center>
+		<form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="get">
+		<table class="Admin" align="center">
+		
+<?php 
+$db = mysql_connect($admin_db_address, $admin_db_user_name, $admin_db_password);
+mysql_select_db($admin_db_name, $db);
+mysql_query("SET NAMES 'utf8'");
+
+$usr_grp = array();
+$users = array();
+
+// get users
+$filter = isset($_GET['filter']) ? $_GET['filter'] : null;
+if(isset($filter))
+{
+	$filter = str_replace( "*", "%", $filter);
+	$filter = str_replace( "?", "_", $filter);
+}
+else
+{
+	$filter = "%";
+}
+$filter = mysql_real_escape_string( $filter );
+
+$sql = "SELECT `id`, `user` FROM `user` WHERE `user` LIKE '$filter' ORDER BY `user`.`user`";
+$result = mysql_query($sql, $db);
+
+while ( $row = mysql_fetch_row($result) )
+{
+	$uid = $row[0];
+	$usr_grp[$uid] = array();
+	$users[$uid] = $row[1];
+}
+
+// get groups
+$groups_loc = array();
+$sql = "SELECT `id`, `group` FROM `group` ORDER BY `group`.`group`";
+$result = mysql_query($sql, $db);
+while ( $row = mysql_fetch_row($result) )
+{
+	$gid = $row[0];
+	$usr_grp[$uid][$gid] = false;
+	$groups_loc[$gid] = $row[1];
+}
+
+$sql = "SELECT user_id, group_id FROM `group` INNER JOIN `user_group` ON `group`.id=`group_id`";
+$result = mysql_query($sql, $db);
+while ( $row = mysql_fetch_row($result) )
+{
+	$uid = $row[0];
+	$gid = $row[1];
+	$usr_grp[$uid][$gid] = true;
+}
+
+echo("<th colspan=\"1\" align=\"center\">&nbsp;</th>\r\n");
+foreach($groups_loc as $group)
+{
+	echo("<th colspan=\"1\" align=\"center\">$group</th>\r\n");
+}
+foreach($users as $uid => $local_user)
+{
+	echo("<tr class=\"Admin\">");
+	echo("<td align=\"center\">$local_user</td>\r\n");
+	
+	foreach($groups_loc as $gid => $group)
+	{
+		$checked = "";
+		if(isset($usr_grp[$uid][$gid]) && $usr_grp[$uid][$gid] == true)
+		{
+			$checked = "checked=\"checked\"";
+		}
+		
+		echo("<td align=\"center\"><input type=\"checkbox\" name=\"$uid-$gid\" $checked /></td>\r\n");
+	}
+	echo("</tr>");
+}
+mysql_close();
+		?>
+		
+        </table>
+            <input type="reset" />
+        </form>
+            <br />
+            <form action="./admin/update_users.php" name="update_form" onsubmit="return on_submit(update_form)"
+                method="get">
+                <input type="hidden" name="usr_grps" value="" />
+                <input type="submit" value="Update" />
+            </form>
+        </center>
+            <br />
+            <hr>
+            <!-- Navagation Bar -->
+			<?php 
+			include("./module/bottom_toolbar.php"); 
+			include("./module/contact_info.php");
+            ?>
+            <br />
+            <span style="font-size: smaller;"><em>Version 1.5.0.2 Sat Sep 8 11:23:37 CDT 2007 ~(
+                Copyright © by Brian Preston (2007) )</em> </span>
+       </div>
+    </body>
+</html>
