eccb430e9f684d6d46eff9043dee9b8a84dfa1a2
Brian Preston <brian8181@gmail.com>
2008-11-11 17:10:15 +0000

    add table functions
    git-svn-id: file:///home/brian/mig/music_web/trunk@103 356dc8ab-2228-4c92-b7b5-0cb1f9e44984

---
+++ php/table_functions.php
@@ -0,0 +1,41 @@
+<?php
+function get_picture_cell($album, $artist, $art_location, $file)
+{
+	return "<td>
+				<a class=\"NoColor\" href=\"./results.php?album=$album&artist=$artist&amp;order_by=artist,album,track,title\">
+					<img src=\"$art_location/xsmall/$file\" width=\"50\" height=\"50\" alt=\"NA\"/>
+				</a>
+			</td>";
+}
+function get_track_cell($track)
+{
+	return "<td>$track</td>";	
+}
+function get_title_cell($title, $sid)
+{
+	return "<td>
+			    <a href=\"details.php?sid=$sid\">$title</a>
+			</td>";	
+}
+function get_album_cell($album)
+{
+	return 	"<td>
+			    <a href=\"results.php?album=$album&amp;order_by=artist,album,track,title\">$album</a>
+			</td>";
+}
+function get_artist_cell($artist)
+{
+	return 	"<td>
+			    <a href=\"results.php?artist=$artist&amp;order_by=artist,album,track,title\">$artist</a>
+			</td>";
+}
+function get_order_cell($order)
+{
+	return 	"<td>$order</td>";
+}
+//todo
+function get_checkbox_cell($order)
+{
+	return 	"<td>TODO</td></td>";
+}
+?>
