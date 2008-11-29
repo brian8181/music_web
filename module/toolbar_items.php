c9b78f19bc2104f1e9959d133c73e091b2fa269d
Brian Preston <brian8181@gmail.com>
2008-11-29 22:14:12 +0000

    git-svn-id: file:///home/brian/mig/music_web/trunk@117 356dc8ab-2228-4c92-b7b5-0cb1f9e44984

--- module/toolbar_items.php
+++ module/toolbar_items.php
@@ -2,33 +2,33 @@
 if( isset( $enable_advanced ) && $enable_advanced == true )
 {
 	?>
-	 <td align="center">
+	 <li align="center">
        <a class="Nav" href="adv_query.php">Advanced</a>
-    </td>
+    </li>
 <?php
 }
 if( isset( $enable_browse ) && $enable_browse == true )
 	{
 	?>
-	<td align="center">
+	<li align="center">
        <a class="Nav" href="browse_artist.php?nav_row=0">Browse</a>
-    </td>
+    </li>
 <?php
 }
 if( isset( $enable_playlist ) && $enable_playlist == true )
 {
 		?>
-    <td align="center">
+    <li align="center">
        <a class="Nav" href="playlists.php">Playlists</a>
-     </td>
+     </li>
 <?php
 }
 if( isset( $enable_statistics ) && $enable_statistics == true )
 {
 	?>
-	 <td align="center">
+	 <li align="center">
        <a class="Nav" href="music_stats.php">Statistics</a>
-     </td>
+     </li>
 <?php
 }
 if( isset( $enable_security ) && $enable_security == true )
@@ -36,16 +36,16 @@ if( isset( $enable_security ) && $enable_security == true )
 	if(assert_group('admin'))
 	{
 	?>
-	 <td align="center">
+	 <li align="center">
        <a class="Nav" href="./user_admin.php">Admin</a>
-     </td>
+     </li>
 <?php
 	}
 }
 if( isset( $enable_quick_search ) && $enable_quick_search == true )
 {
 	?>
-	 <td align="center">
+	 <li align="center">
         <form name="toolbar" onsubmit="on_quick_submit(toolbar)" method="get" action="results.php">
           <input type="hidden" name="artist" />
           <input type="hidden" name="title" />
@@ -57,7 +57,7 @@ if( isset( $enable_quick_search ) && $enable_quick_search == true )
             <input type="text" name="album" align="right"/>
           </div>
         </form>
-      </td>
+      </li>
 <?php
 }
 ?>
